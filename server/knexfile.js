8e3e5a66e79012bc48521a3d38700a0c271feccf
thientam075 <72021036+thientam075@users.noreply.github.com>
2021-10-27 13:45:58 +0700

    Config DB production

--- server/knexfile.js
+++ server/knexfile.js
@@ -28,10 +28,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: process.env.DB_DB,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
+      database: "db4joun5q12mqc",
+      user: "ihkpdgdaembbvd",
+      password: "33390c0664c9c3b5d4946bf94d76e66814cde8b9d9a7a2ebf0b693c21e9760a8",
+      host: "ec2-54-210-226-209.compute-1.amazonaws.com",
+      ssl: {rejectUnauthorized: false},
     },
     migrations: {
       directory: './data/migrations',
